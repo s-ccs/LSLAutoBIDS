0b4307b0a8f1599f9376cd8faf4495005923d13e
ReboreExplore <manpabarmanaec@gmail.com>
2023-05-24 09:20:32 +0200

    added docstrings

--- test_folder_dir/main.py
+++ test_folder_dir/main.py
@@ -51,21 +51,59 @@ def check_for_new_file(folder_path, last_checked_file_path):
     
 # Load the xdf file from the given path and get its streams    
 def get_the_streams(xdf_path):
+    """
+    Retrieve the stream names and information from an XDF file.
+
+    Parameters:
+    xdf_path (str): The path to the XDF file.
+
+    Returns:
+    tuple: A tuple containing the stream names and the stream information.
+
+    """
+        
     streams = resolve_streams(xdf_path)
     stream_names = [streams[i]['name'] for i in range(len(streams))]
     return stream_names,streams
 
 
 def create_raw_xdf(xdf_path,streams):
+    """
+    Create a raw object from an XDF file containing specific streams.
+
+    Parameters:
+    xdf_path (str): The path to the XDF file.
+    streams (list): A list representing the streams extracted from the xdf file.
+
+    Returns:
+    mne.io.RawXDF: The raw object created from the XDF file.
+
+    """
     # Get the stream id of the EEG stream
     stream_id = match_streaminfos(streams, [{"type": "EEG"}])[0]
     raw = read_raw_xdf(xdf_path,stream_ids=[stream_id])
     return raw
 
+# Upload to DARUS
+def upload_to_darus(file_path):
+    # Upload to DARUS
+    # Set the DARUS upload path
+    return 0
+
 
 # Record or update the file in the database
 
 def check_input_validity(number):
+    """
+    Check the validity of a number input and prompt for a valid three-digit number.
+
+    Parameters:
+    number (str): The number input to check.
+
+    Returns:
+    str: The valid three-digit number.
+
+    """
     while True:
         if number.isdigit() and len(number) == 3:
             return number
@@ -74,6 +112,15 @@ def check_input_validity(number):
             number = input("Enter a valid number: ")
             
 def record_fresh_data():
+    """
+    Record fresh data and save it in the BrainVision BIDS format.
+
+    This function prompts the user to enter the subject ID, session ID, and task name. 
+    It then creates the necessary directory structure in the BIDS format, creates a raw object from an XDF file,
+    and writes the raw data to BIDS in the BrainVision format.
+
+    """
+
     # Code to record fresh data
     print("Recording fresh data...")
     
@@ -116,6 +163,11 @@ def record_fresh_data():
     # Do you want to upload to DaRUS now
 
 def check_for_new_data():
+    """
+    This function checks for new data by comparing the current state of the BIDS_ROOT directory with the last checked
+    state. If new files are found, it uploads them to DaRUS. It also keeps a log of the file changes in a text file.
+    
+    """
     # Code to check for new data
     print("Checking for new data...")
 
@@ -132,6 +184,14 @@ def check_for_new_data():
         
 # Main Function
 def main():
+    """
+    Main function to interact with the user and perform actions based on user input.
+
+    This function prompts the user to choose between recording fresh data or checking for new data. Based on the user's
+    input, it calls the respective functions (`record_fresh_data` or `check_for_new_data`). The program continues to
+    prompt for user input until the user chooses to quit by entering 'q'.
+
+    """
     while True:
         user_input = input("Enter '1' to record fresh data or '2' to check for new data (press 'q' to quit): ")
 
