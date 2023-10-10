12f80ab7289d5dc8504e9f09820c74482273e6d1
ReboreExplore <manpabarmanaec@gmail.com>
2023-10-10 09:54:48 +0200

    update new branch

--- lsl_autobids/folder_config.py
+++ lsl_autobids/folder_config.py
@@ -1,13 +1,15 @@
-# config.py
-
+import os
 # Set up the BIDS output path
-BIDS_ROOT = './data/bids/'
+BIDS_ROOT = '../data/bids/'
 
 # This is the actual path to the directory containing xdf files
-PROJECT_ROOT = './data/projects/'
-
-# Project name
-PROJECT_NAME = 'sampleproject'
+PROJECT_ROOT = '../data/projects/'
 
 # This is the actual path to the directory containing the stimulus files
-PROJECTS_STIM_ROOT = './data/project_stimulus/'
+PROJECTS_STIM_ROOT = '../data/project_stimulus/'
+
+def list_directories(path=PROJECT_ROOT):
+    directories = [d for d in os.listdir(path) if os.path.isdir(os.path.join(path, d))]
+    return directories
+
+PROJECTS = list_directories(PROJECT_ROOT)
