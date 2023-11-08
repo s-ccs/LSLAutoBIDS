8dfcc207d3027cfc2590819d0520f258ba09a669
Manpa Barman <manpabarmanaec@gmail.com>
2023-11-08 12:07:48 +0100

    take generation paths from yaml file

--- gen_dataset_config.py
+++ gen_dataset_config.py
@@ -2,6 +2,15 @@
 import yaml
 import argparse
 
+def parse_yaml_file(yaml_file):
+    with open(yaml_file, 'r') as file:
+        try:
+            data = yaml.safe_load(file)
+            return data
+        except yaml.YAMLError as e:
+            print(f"Error parsing YAML file: {e}")
+            return None
+
 argparser = argparse.ArgumentParser(description='Get the project name')
 argparser.add_argument('-p','--project_name', type=str, help='Enter the project name')
 args = argparser.parse_args()
@@ -14,7 +23,10 @@ data = {
     "project_title": 'default project title',
 }
 
-yaml_filename = 'data/projects/'+project_name+'/dataset_data.yaml'
+config_file = 'data_config.yaml'
+config = parse_yaml_file(config_file)
+project_root = config['PROJECT_ROOT']
+yaml_filename = project_root +project_name+'/dataset_data.yaml'
 
 # Write the data to the YAML file
 with open(yaml_filename, "w") as yaml_file:
