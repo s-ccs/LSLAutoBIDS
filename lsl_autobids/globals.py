835ed0ad3d75821369421f0b169ac1875bc6524a
Manpa Barman <manpabarmanaec@gmail.com>
2024-07-03 11:57:46 +0200

    added globals file

---
+++ lsl_autobids/globals.py
@@ -0,0 +1,32 @@
+import os
+import yaml
+
+def parse_yaml_file(yaml_file):
+    """
+    Parse a YAML file and return the data
+
+    Parameters
+    ----------
+    yaml_file : str
+        Path to the root config YAML file
+
+    Returns
+    -------
+    dict
+        Data from the YAML file
+    """
+    with open(yaml_file, 'r') as file:
+        try:
+            data = yaml.safe_load(file)
+            return data
+        except yaml.YAMLError as e:
+            print(f"Error parsing YAML file: {e}")
+            return None
+
+
+config_file = os.path.join(os.path.expanduser("~"),'.config/lslautobids/autobids_config.yaml')
+config = parse_yaml_file(config_file)
+
+project_root = os.path.join(os.path.expanduser("~"),config['PROJECT_ROOT'])
+bids_root = os.path.join(os.path.expanduser("~"),config['BIDS_ROOT'])
+project_stim_root = os.path.join(os.path.expanduser("~"),config['PROJECT_STIM_ROOT'])
