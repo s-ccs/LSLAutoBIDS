630ecdbe290429e282fa9a2c077a62bd87abced5
Recorder Laptop <recorder@s-ccs.com>
2024-09-18 15:47:26 +0200

    modify gitattributes only once, better message

--- lsl_autobids/datalad_create.py
+++ lsl_autobids/datalad_create.py
@@ -3,15 +3,19 @@ import os
 
 
 def create_and_add_files_to_datalad_dataset(dataset_path,flag):
+    message = "LSL Auto BIDS: new files found and added"
     if flag==0:
+    	message ="LSL Auto BIDS: new datalad dataset created"
         # Create a new dataset
         print('Creating a new datalad dataset........')
         dl.create(dataset_path, force=True)
     # Commit changes
     # Change to dataset path
     os.chdir(dataset_path)
-    with open(os.path.join(dataset_path,".gitattributes"), "a") as f:
-        f.write("* annex.largefiles=nothing")
+    if flag==0:
+    	# needed to modify participants.tsv etc. later
+    	with open(os.path.join(dataset_path,".gitattributes"), "a") as f:
+        	f.write("* annex.largefiles=nothing")
     print('Committing current changes........')
-    dl.save(path = '.', message="First BIDS upload test")
+    dl.save(path = '.', message=message)
     
