cebd4312a74847c8f16b8d9858a2ebd0ff3452f7
Recorder Laptop <recorder@s-ccs.com>
2024-09-18 14:46:46 +0200

    added a modification to not git annex the overall dataset files; this should remove the need to unlock the dataset

--- lsl_autobids/datalad_create.py
+++ lsl_autobids/datalad_create.py
@@ -10,6 +10,8 @@ def create_and_add_files_to_datalad_dataset(dataset_path,flag):
     # Commit changes
     # Change to dataset path
     os.chdir(dataset_path)
+    with open(os.path.join(dataset_path,".gitattributes"), "a") as f:
+        f.write("* annex.largefiles=nothing")
     print('Committing current changes........')
     dl.save(path = '.', message="First BIDS upload test")
-
+    
