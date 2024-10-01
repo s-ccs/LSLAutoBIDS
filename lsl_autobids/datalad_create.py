55cead1072d5d6ee8dd6762b76495eb1a36ac4a3
Recorder Laptop <recorder@s-ccs.com>
2024-10-01 13:11:49 +0200

    fix intend bug

--- lsl_autobids/datalad_create.py
+++ lsl_autobids/datalad_create.py
@@ -5,7 +5,7 @@ import os
 def create_and_add_files_to_datalad_dataset(dataset_path,flag):
     message = "LSL Auto BIDS: new files found and added"
     if flag==0:
-    	message ="LSL Auto BIDS: new datalad dataset created"
+        message ="LSL Auto BIDS: new datalad dataset created"
         # Create a new dataset
         print('Creating a new datalad dataset........')
         dl.create(dataset_path, force=True)
@@ -15,7 +15,7 @@ def create_and_add_files_to_datalad_dataset(dataset_path,flag):
     if flag==0:
     	# needed to modify participants.tsv etc. later
     	with open(os.path.join(dataset_path,".gitattributes"), "a") as f:
-        	f.write("* annex.largefiles=nothing")
+    	    f.write("* annex.largefiles=nothing")
     print('Committing current changes........')
     dl.save(path = '.', message=message)
     
