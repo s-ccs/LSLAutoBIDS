48a4a497ef0c314338d655941538b64c0204facc
behinger (s-ccs 001) <benedikt.ehinger@vis.uni-stuttgart.de>
2025-01-13 12:01:12 +0100

    tab to spaces

--- lsl_autobids/datalad_create.py
+++ lsl_autobids/datalad_create.py
@@ -15,7 +15,7 @@ def create_and_add_files_to_datalad_dataset(dataset_path,flag):
     if flag==0:
     	# needed to modify participants.tsv etc. later
     	with open(os.path.join(dataset_path,".gitattributes"), "a") as f:
-    	    f.write("* annex.largefiles=nothing")
+            f.write("* annex.largefiles=nothing")
     print('Committing current changes........')
     dl.save(path = '.', message=message)
     
