f7a2e1659554e7c29355323e9d93153d92a655ae
Manpa Barman <manpa.barman97@gmail.com>
2025-07-11 16:52:12 +0200

    initial setup for pip installable

--- setup.py
+++ setup.py
@@ -1,34 +1,16 @@
 from setuptools import setup, find_packages
 
-# with open('requirements.txt') as f:
-#     requirements = f.read().splitlines()
+with open('requirements.txt') as f:
+    requirements = f.read().splitlines()
 setup(
-    name='LSLAutoBIDS',
+    name='lsl_autobids',
     version='0.1.0',
     description='Tools to convert LSL + friends automatically to BIDS, and upload to a Dataverse',
-    author='Your Name',
-    author_email='your.email@example.com',
+    author='Manpa Barman, Benedikt Ehinger',
+    author_email='manpa.barman97@gmail.com, science@benediktehinger.de',
     url='https://github.com/s-ccs/LSLAutoBIDS',
     packages=find_packages(include=['lsl_autobids', 'lsl_autobids.*']),
-    install_requires= #requirements,
-    [
-        # List your dependencies here, e.g.,
-        'pyxdf',
-        'mne',
-        'mne-bids',
-        'bids_validator==1.13.1',
-        'datalad-dataverse==1.0.1',
-        'datalad-installer==1.0.3',
-        'pyDataverse==0.3.1',
-        'requests>=2.12.0',
-        'jsonschema>=3.2.0',
-        'AnnexRemote@git+https://github.com/Lykos153/AnnexRemote.git@master#egg=AnnexRemote',
-        'toml',
-        'pyyaml',
-        'mnelab',
-        'pybv',
-        'pytest',
-    ],
+    install_requires= requirements,
     classifiers=[
         'Programming Language :: Python :: 3',
         'License :: OSI Approved :: MIT License',
